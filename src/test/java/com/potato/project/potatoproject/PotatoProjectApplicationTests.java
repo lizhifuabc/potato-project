0f94b1e6dbd77667fb35ad4e9a0e1cc5916f2345
lizhifu <lizhifuabc@163.com>
2021-05-19 09:27:00 +0800

    fisrt

---
+++ src/test/java/com/potato/project/potatoproject/PotatoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.potato.project.potatoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PotatoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
